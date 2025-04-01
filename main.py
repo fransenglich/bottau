9bc15c96a29ef9bcc0076c44db5820e59965394d
Frans Englich <fenglich@fastmail.fm>
2025-04-01 16:53:34 +0200

    Experiment.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def initialDownload() -> None:
         #df = yf.download(ticker, period='1y', interval='1d', auto_adjust=False)
         df = yf.download(ticker, period='1y', group_by='Ticker', interval='1d', auto_adjust=False)
         df = df.stack(level=0, future_stack=True).rename_axis(['Date', 'Ticker']).reset_index(level=1)
-        df['ticker'] = ticker
+        #df['ticker'] = ticker
         df.to_csv(f'Tickers/{ticker}.csv')
         df_tickers.append(df)
 
@@ -24,18 +24,22 @@ def fetchNewTicks() -> None:
     for ticker in tickers:
         df = pd.read_csv(f'Tickers/{ticker}.csv')
 
-        tmp = df['Date'].tail(1).item()
-        startDate = (datetime.strptime(tmp, '%Y-%m-%d') + timedelta(days=1)).strftime('%Y-%m-%d')
+        date_item = df['Date'].tail(1).item()
+        startDate = (datetime.strptime(date_item, '%Y-%m-%d') + timedelta(days=1)).strftime('%Y-%m-%d')
 
         print("FOO: " + str(startDate))
 
         updated_df = yf.download(ticker, period='1y', group_by='Ticker', interval='1d', auto_adjust=False, start=startDate)
         updated_df = updated_df.stack(level=0, future_stack=True).rename_axis(['Date', 'Ticker']).reset_index(level=1)
-        updated_df['ticker'] = ticker
+        #updated_df['ticker'] = ticker
+
+        print(df)
 
         print(updated_df)
 
         merged = pd.concat([df, updated_df])
+
+        #print(merged)
 
         merged.to_csv(f'Tickers/{ticker}_merged.csv')
 
