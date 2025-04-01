6b9ee0d4f22ec4100b392f46631eddf43f59fdaf
Frans Englich <fenglich@fastmail.fm>
2025-04-01 16:22:37 +0200

    Initial commit, track.

---
+++ main.py
@@ -0,0 +1,55 @@
+import yfinance as yf
+import sys
+import pandas as pd
+from datetime import datetime, timedelta
+
+tickers = ['AAPL', 'TSLA']
+df_tickers = []
+
+# We build simple DataFrames, one header like what is typical.
+# Discussed here:
+#   https://stackoverflow.com/questions/63107594/how-to-deal-with-multi-level-column-names-downloaded-with-yfinance/63107801#63107801
+#
+def initialDownload() -> None:
+    for ticker in tickers:
+        #df = yf.download(ticker, period='1y', interval='1d', auto_adjust=False)
+        df = yf.download(ticker, period='1y', group_by='Ticker', interval='1d', auto_adjust=False)
+        df = df.stack(level=0, future_stack=True).rename_axis(['Date', 'Ticker']).reset_index(level=1)
+        df['ticker'] = ticker
+        df.to_csv(f'Tickers/{ticker}.csv')
+        df_tickers.append(df)
+
+
+def fetchNewTicks() -> None:
+    for ticker in tickers:
+        df = pd.read_csv(f'Tickers/{ticker}.csv')
+
+        tmp = df['Date'].tail(1).item()
+        startDate = (datetime.strptime(tmp, '%Y-%m-%d') + timedelta(days=1)).strftime('%Y-%m-%d')
+
+        print("FOO: " + str(startDate))
+
+        updated_df = yf.download(ticker, period='1y', group_by='Ticker', interval='1d', auto_adjust=False, start=startDate)
+        updated_df = updated_df.stack(level=0, future_stack=True).rename_axis(['Date', 'Ticker']).reset_index(level=1)
+        updated_df['ticker'] = ticker
+
+        print(updated_df)
+
+        merged = pd.concat([df, updated_df])
+
+        merged.to_csv(f'Tickers/{ticker}_merged.csv')
+
+        df_tickers.append(merged)
+
+
+def main() -> int:
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "i":
+            initialDownload()
+        elif sys.argv[1] == "c":
+            fetchNewTicks()
+
+    return 0
+
+
+main()
