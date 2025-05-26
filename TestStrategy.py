f1ef0232d12efa7cd381f21ffd1f0f7db2b2a8a2
Frans Englich <fenglich@fastmail.fm>
2025-05-26 20:04:33 +0200

    Boiler plate code for testing, refactor.

---
+++ TestStrategy.py
@@ -0,0 +1,20 @@
+import unittest
+
+class TestStrategy(unittest.TestCase):
+
+    def test_internetconnected(self):
+        pass
+    
+    def test_initialize(self):
+        pass
+
+    def test_main(self):
+        pass
+
+    def test_external_exec(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+    
