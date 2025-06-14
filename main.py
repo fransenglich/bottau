6d1c758e058e507c1d4f68c09df918e619fae0f2
Frans Englich <fenglich@fastmail.fm>
2025-06-14 19:50:54 +0200

    Plot a correlation matrix of features. Rendered with latest matplotlib.

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
+import matplotlib
 import sys
 import ta
+import quantreo.features_engineering as fe
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
 
 tickers = ['AAPL', 'TSLA']
-
 def_figsize = (6, 4)
 transaction_commission = 0.02
+rolling_window_size = 30
 
 # Data Frames of tickers, comes from Yahoo Finance and are in classic OHLC(Adj)V
 # format.
@@ -60,6 +62,123 @@ def fetchNewTicks() -> None:
 # TODO:
 # - Fix fetchNewTicks()
 
+# Taken from https://matplotlib.org/stable/gallery/images_contours_and_fields/image_annotated_heatmap.html
+def heatmap(data, row_labels, col_labels, ax=None,
+            cbar_kw=None, cbarlabel="", **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Parameters
+    ----------
+    data
+        A 2D numpy array of shape (M, N).
+    row_labels
+        A list or array of length M with the labels for the rows.
+    col_labels
+        A list or array of length N with the labels for the columns.
+    ax
+        A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
+        not provided, use current Axes or create a new one.  Optional.
+    cbar_kw
+        A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
+    cbarlabel
+        The label for the colorbar.  Optional.
+    **kwargs
+        All other arguments are forwarded to `imshow`.
+    """
+
+    if ax is None:
+        ax = plt.gca()
+
+    if cbar_kw is None:
+        cbar_kw = {}
+
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # Show all ticks and label them with the respective list entries.
+    ax.set_xticks(range(data.shape[1]), labels=col_labels,
+                  rotation=-30, ha="right", rotation_mode="anchor")
+    ax.set_yticks(range(data.shape[0]), labels=row_labels)
+
+    # Let the horizontal axes labeling appear on top.
+    ax.tick_params(top=True, bottom=False,
+                   labeltop=True, labelbottom=False)
+
+    # Turn spines off and create white grid.
+    ax.spines[:].set_visible(False)
+
+    ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
+    ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
+    ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+    ax.tick_params(which="minor", bottom=False, left=False)
+
+    return im, cbar
+
+
+# Taken from https://matplotlib.org/stable/gallery/images_contours_and_fields/image_annotated_heatmap.html
+def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
+                     textcolors=("black", "white"),
+                     threshold=None, **textkw):
+    """
+    A function to annotate a heatmap.
+
+    Parameters
+    ----------
+    im
+        The AxesImage to be labeled.
+    data
+        Data used to annotate.  If None, the image's data is used.  Optional.
+    valfmt
+        The format of the annotations inside the heatmap.  This should either
+        use the string format method, e.g. "$ {x:.2f}", or be a
+        `matplotlib.ticker.Formatter`.  Optional.
+    textcolors
+        A pair of colors.  The first is used for values below a threshold,
+        the second for those above.  Optional.
+    threshold
+        Value in data units according to which the colors from textcolors are
+        applied.  If None (the default) uses the middle of the colormap as
+        separation.  Optional.
+    **kwargs
+        All other arguments are forwarded to each call to `text` used to create
+        the text labels.
+    """
+
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+
+    # Normalize the threshold to the images color range.
+    if threshold is not None:
+        threshold = im.norm(threshold)
+    else:
+        threshold = im.norm(data.max())/2.
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(horizontalalignment="center",
+              verticalalignment="center")
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)])
+            text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
+            texts.append(text)
+
+    return texts
+
 def downloadToFile():
     df = yf.download("AAPL", interval='1d').loc["2015":]
     df.to_csv("Tickers/AAPL.manual.csv")
@@ -67,8 +186,9 @@ def downloadToFile():
 def main() -> int:
     global df_tickers
     global def_figsize
+    global rolling_window_size
 
-    def savefig(plt, basename):
+    def savefig(plt: matplotlib.figure.Figure, basename: str) -> None:
         plt.savefig(f"generated/{basename}.png")
 
     if len(sys.argv) == 2:
@@ -87,7 +207,6 @@ def main() -> int:
         df_tickers.append(df)
 
     df: pd.DataFrame = df_tickers[0]
-
 
     # For some reason the name differs.
     df = df.rename(columns = {"1. open":    "Open",
@@ -126,6 +245,7 @@ def main() -> int:
     df["pct_close_futur"] = (df["Close"].shift(-2)-df["Close"])/df["Close"]
 
     # Compute the returns of each position
+    # This is our computed trading signal applied to the returns
     df["returns"] = df["signal"]*df["pct_close_futur"]
 
     print(df)
@@ -207,6 +327,38 @@ def main() -> int:
     plt.legend()
     plt.grid()
     savefig(plt, "cumulative_returns")
+
+
+    # ---- corr matrix ----
+    indicators: pd.DataFrame = []
+    indicators.append(df['pct_close_futur'])
+    df['var'] = df['pct_close_futur'].rolling(window = rolling_window_size).var()
+    df['parkinsons_var'] = fe.volatility.parkinson_volatility(df, window_size=rolling_window_size,
+                                                              high_col = "High", low_col="Low")
+    indicators.append(df['var'])
+    indicators.append(df['parkinsons_var'])
+
+    ilen = len(indicators)
+    in_range = range(ilen)
+    corrmatrix = np.zeros((ilen, ilen), dtype = float)
+
+    for i in in_range:
+        # This works: for l in range(ilen - (ilen - i) + 1):
+        for l in in_range:
+            corrmatrix[i, l] = indicators[i].corr(indicators[l])
+    
+    cm_labels = [i.name for i in indicators]
+
+    fig, ax = plt.subplots()
+    im, cbar = heatmap(corrmatrix, cm_labels, cm_labels, ax=ax,
+                       cmap="YlGn", cbarlabel="Correlation coefficient")
+    texts = annotate_heatmap(im, valfmt="{x:.1f} t")
+
+    fig.tight_layout()
+
+    ax.set_title("Heatmap of correlation matrix of features and targets")
+    savefig(fig, "corrmatrix")
+
 
     # ---- Cumulative Returns Minus Transcation Costs ----
     def transaction_cost(trade):
