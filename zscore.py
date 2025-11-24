5abe2ff2f6fdf97641e5c444a81d224596a78a3d
Frans Englich <fenglich@fastmail.fm>
2025-11-24 13:15:52 +0100

    First attempt at rolling Z-score, probably wrong.

--- zscore.py
+++ zscore.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import scipy.stats
 
@@ -11,10 +12,17 @@ def main() -> None:
 
     df["zscore"] = scipy.stats.zscore(df["4. close"])
 
+    df["zscore_rolling30"] = np.nan
+    col = df.columns.get_loc("zscore_rolling30")
+
+    for i in range(len(df)):
+        df.iloc[i, col] = scipy.stats.zscore(df["4. close"].iloc[i:(i + 30)])[-1]
+
     plt.figure(figsize=(15, 8))
     plt.plot(df["zscore"])
-    plt.legend(["Z-scores"])
-    plt.df = df[::-1]
+    plt.plot(df["zscore_rolling30"])
+    plt.legend(["Z-score", "Z-score 30 days"])
+
     plt.title("Z-scores for closing prices")
     plt.grid(axis='y', linestyle='-', alpha=0.5, color='lightgrey')
     plt.show()
