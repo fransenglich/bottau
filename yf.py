df4058015652dd346e49ef24f143665ce1ef45a2
Frans Englich <fenglich@fastmail.fm>
2025-12-07 10:52:24 +0100

    How to grab data from MultiIndex.

--- yf.py
+++ yf.py
@@ -1,11 +1,22 @@
-
 import yfinance as yf
 import pandas as pd
 
-def main() -> None:
-    df: pd.DataFrame = yf.download("^GSPC", multi_level_index=False)
 
-    df.to_csv("Tickers/GSPC.csv")
+def main2() -> None:
+    df = pd.read_csv("Tickers/AAPL orig.csv")
+    print(df)
+
+
+def main() -> None:
+    df: pd.DataFrame = yf.download(("AAPL", "NVDA"), multi_level_index=True)
+
+    print(df)
+    df.to_csv("Tickers/multiindex.csv")
+
+    df2 = df["Close"]["AAPL"]
+    print(df2)
+    df2.to_csv("Tickers/df.csv")
+
 
 if __name__ == "__main__":
     main()
