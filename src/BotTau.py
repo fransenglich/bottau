686561a49d1269ff949a9ea5c737e52347cb8265
Frans Englich <fenglich@fastmail.fm>
2026-01-05 13:26:01 +0100

    Factor out ML to own function.

--- src/BotTau.py
+++ src/BotTau.py
@@ -2,115 +2,20 @@ import sys
 
 from scipy.optimize import minimize
 from sklearn.linear_model import LinearRegression
-from statsmodels.stats.outliers_influence import variance_inflation_factor
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import quantreo.features_engineering as fe
 import quantreo.target_engineering as te
 import statsmodels.api as sm
 
 from lib import backtest
 from lib import common
 from lib import download
-from lib import heatmap
+from lib import ml_modelling
 from strategies.bollinger_rsi import strategy_Bollinger_RSI
 
 # Data Frames of tickers, comes from Yahoo Finance and are in classic OHLC(Adj)V
 # format.
 df_tickers = []
-
-
-def ml_investigate(df: pd.DataFrame, featurenames: tuple[str]) -> pd.DataFrame:
-    """Writes data and graphs that are strategy-agnostic.
-    
-    This is specific to features used in regressions."""
-
-    # This is our design matrix.
-    designmatrix: pd.DataFrame = pd.DataFrame()
-
-    for name in featurenames:
-        designmatrix[name] = df[name]
-
-    # We do 3 things: add standard features, produce correlation matrix and VIF.
-    # ---- corr matrix ----
-
-    designmatrix['pct_close_futur'] = df['pct_close_futur']
-
-    df['var'] = df['pct_close_futur'].rolling(window=common.WINDOW_SIZE).var()
-    designmatrix['var'] = df['var']
-
-    df['vol_parkinson_30'] = fe.volatility.parkinson_volatility(df,
-                                                                high_col="High",
-                                                                low_col="Low",
-                                                                window_size=30)
-    designmatrix['vol_parkinson_30'] = df['vol_parkinson_30']
-
-    df['vol_parkinson_60'] = fe.volatility.parkinson_volatility(df,
-                                                                high_col="High",
-                                                                low_col="Low",
-                                                                window_size=60)
-    designmatrix['vol_parkinson_60'] = df['vol_parkinson_60']
-
-    df['vol_ctc_30'] = fe.volatility.close_to_close_volatility(df,
-                                                               high_col="High",
-                                                               low_col="Low",
-                                                               window_size=30)
-    designmatrix['vol_ctc_30'] = df['vol_ctc_30']
-
-    df['vol_ctc_60'] = fe.volatility.close_to_close_volatility(df,
-                                                               high_col="High",
-                                                               low_col="Low",
-                                                               window_size=60)
-    designmatrix['vol_ctc_60'] = df['vol_ctc_60']
-
-    flen = len(designmatrix.columns)
-    in_range = range(flen)
-    pearsonmatrix = np.zeros((flen, flen), dtype=float)
-    spearmanmatrix = np.zeros((flen, flen), dtype=float)
-
-    for i in in_range:
-        # This works: for l in range(ilen - (ilen - i) + 1):
-        for length in in_range:
-            pearsonmatrix[i, length] = designmatrix.iloc[:, i].corr(designmatrix.iloc[:, length]) # TODO why to column length?
-            spearmanmatrix[i, length] = designmatrix.iloc[:, i].corr(designmatrix.iloc[:, length], method='spearman')
-
-    cm_labels = designmatrix.columns
-
-    # - Pearson
-    fig, ax = plt.subplots()
-    pm, _ = heatmap.heatmap(pearsonmatrix, cm_labels, cm_labels, ax=ax,
-                            cmap="YlGn", cbarlabel="Pearson correlation coefficient")
-    heatmap.annotate_heatmap(pm)
-
-    ax.set_title("Heatmap of Pearson correlation matrix of features")
-    fig.tight_layout()
-    common.savefig(fig, "pearsonmatrix")
-
-    # - Spearman
-    fig, ax = plt.subplots()
-    pm, _ = heatmap.heatmap(spearmanmatrix, cm_labels, cm_labels, ax=ax,
-                            cmap="YlGn", cbarlabel="Spearman correlation coefficient")
-    heatmap.annotate_heatmap(pm)
-
-    ax.set_title("Heatmap of Spearman correlation matrix of features")
-    fig.tight_layout()
-    common.savefig(fig, "spearmanmatrix")
-
-    # - Multicollinearity
-
-    # variance_inflation_factor() needs this.
-    designmatrix.dropna(inplace=True)
-
-    vifs = [(designmatrix.columns.values[i],
-             variance_inflation_factor(designmatrix, i))
-             for i in range(len(designmatrix.columns))]
-
-    with open("generated/VIFs.tex", "w") as f:
-        for name, vif in vifs:
-            f.write(name.replace("_", "\\_") + " & " + str(round(vif, 2)) + " \\\\\n")
-
-    return df
 
 
 def test_opt_Bollinger_RSI(df: pd.DataFrame) -> None:
@@ -154,6 +59,7 @@ def investigate(df: pd.DataFrame) -> None:
  
 
 def main() -> int:
+    # TODO This function does too much. Attempts live data loading etc.
 
     plt.ioff()
 
@@ -229,7 +135,7 @@ def main() -> int:
         f.write(str(est2.summary()))
 
     df.to_csv("generated/df.csv")
-    ml_investigate(df, ())
+    ml_modelling.investigate(df, ())
 
     backtest.backtest(df)
 
---
+++ src/lib/ml_modelling.py
@@ -0,0 +1,104 @@
+import quantreo.features_engineering as fe
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+import pandas as pd
+
+from lib import common
+from lib import heatmap
+
+"""
+This file contains functions specific to ML models.
+"""
+
+
+def investigate(df: pd.DataFrame, featurenames: tuple[str]) -> pd.DataFrame:
+    """Writes data and graphs that are strategy-agnostic.
+    
+    This is specific to features used in regressions."""
+
+    # This is our design matrix.
+    designmatrix: pd.DataFrame = pd.DataFrame()
+
+    for name in featurenames:
+        designmatrix[name] = df[name]
+
+    # We do 3 things: add standard features, produce correlation matrix and VIF.
+    # ---- corr matrix ----
+
+    designmatrix['pct_close_futur'] = df['pct_close_futur']
+
+    df['var'] = df['pct_close_futur'].rolling(window=common.WINDOW_SIZE).var()
+    designmatrix['var'] = df['var']
+
+    df['vol_parkinson_30'] = fe.volatility.parkinson_volatility(df,
+                                                                high_col="High",
+                                                                low_col="Low",
+                                                                window_size=30)
+    designmatrix['vol_parkinson_30'] = df['vol_parkinson_30']
+
+    df['vol_parkinson_60'] = fe.volatility.parkinson_volatility(df,
+                                                                high_col="High",
+                                                                low_col="Low",
+                                                                window_size=60)
+    designmatrix['vol_parkinson_60'] = df['vol_parkinson_60']
+
+    df['vol_ctc_30'] = fe.volatility.close_to_close_volatility(df,
+                                                               high_col="High",
+                                                               low_col="Low",
+                                                               window_size=30)
+    designmatrix['vol_ctc_30'] = df['vol_ctc_30']
+
+    df['vol_ctc_60'] = fe.volatility.close_to_close_volatility(df,
+                                                               high_col="High",
+                                                               low_col="Low",
+                                                               window_size=60)
+    designmatrix['vol_ctc_60'] = df['vol_ctc_60']
+
+    flen = len(designmatrix.columns)
+    in_range = range(flen)
+    pearsonmatrix = np.zeros((flen, flen), dtype=float)
+    spearmanmatrix = np.zeros((flen, flen), dtype=float)
+
+    for i in in_range:
+        # This works: for l in range(ilen - (ilen - i) + 1):
+        for length in in_range:
+            pearsonmatrix[i, length] = designmatrix.iloc[:, i].corr(designmatrix.iloc[:, length]) # TODO why to column length?
+            spearmanmatrix[i, length] = designmatrix.iloc[:, i].corr(designmatrix.iloc[:, length], method='spearman')
+
+    cm_labels = designmatrix.columns
+
+    # - Pearson
+    fig, ax = plt.subplots()
+    pm, _ = heatmap.heatmap(pearsonmatrix, cm_labels, cm_labels, ax=ax,
+                            cmap="YlGn", cbarlabel="Pearson correlation coefficient")
+    heatmap.annotate_heatmap(pm)
+
+    ax.set_title("Heatmap of Pearson correlation matrix of features")
+    fig.tight_layout()
+    common.savefig(fig, "pearsonmatrix")
+
+    # - Spearman
+    fig, ax = plt.subplots()
+    pm, _ = heatmap.heatmap(spearmanmatrix, cm_labels, cm_labels, ax=ax,
+                            cmap="YlGn", cbarlabel="Spearman correlation coefficient")
+    heatmap.annotate_heatmap(pm)
+
+    ax.set_title("Heatmap of Spearman correlation matrix of features")
+    fig.tight_layout()
+    common.savefig(fig, "spearmanmatrix")
+
+    # - Multicollinearity
+
+    # variance_inflation_factor() needs this.
+    designmatrix.dropna(inplace=True)
+
+    vifs = [(designmatrix.columns.values[i],
+             variance_inflation_factor(designmatrix, i))
+             for i in range(len(designmatrix.columns))]
+
+    with open("generated/VIFs.tex", "w") as f:
+        for name, vif in vifs:
+            f.write(name.replace("_", "\\_") + " & " + str(round(vif, 2)) + " \\\\\n")
+
+    return df
