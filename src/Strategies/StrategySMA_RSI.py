bcf31e4f0663f8d08ccb1ab747fde8ab996ff7ea
Frans Englich <fenglich@fastmail.fm>
2026-01-27 17:29:48 +0100

    Work

--- src/Strategies/StrategySMA_RSI.py
+++ src/Strategies/StrategySMA_RSI.py
@@ -6,7 +6,10 @@ from lib.AbstractStrategy import AbstractStrategy
 class StrategySMA_RSI(AbstractStrategy):
     """A SMA and RSI technical analysis-strategy."""
 
-    # FEATURE 1: SMA 30
     def compute_features(self):
         self.data["SMA_30"] = ta.trend.SMAIndicator(self.data["close"], 30) \
-                                       .sma_indicator()
+            .sma_indicator()
+        self.data["SMA_5"] = ta.trend.SMAIndicator(self.data["close"], 5) \
+            .sma_indicator()
+
+        condition = self.data['SMA5'] > self.data['SMA30']
